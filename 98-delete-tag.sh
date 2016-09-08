ada2a105fd1b403d82a7c9528eb1a792d8be8931
Andreas Blaesius <skate4life@gmx.de>
2016-09-08 22:36:59 +0200

    Add a script to delete a tag from github

---
+++ 98-delete-tag.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Your build source code directory path. In the example below the build source code directory path is in the "home" folder. If your source code directory is on an external HDD it should look like: //media/your PC username/the name of your storage device/path/to/your/source/code/folder
+SAUCE=~/android/aosp-6.0
+
+echo "----------------------------------------"
+echo ""
+echo "           D E L E T E - T A G          "
+echo "            OMAP4-AOSP PROJECT          "
+echo ""
+echo "----------------------------------------"
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/bionic
+git push --delete https://github.com/OMAP4-AOSP/android_bionic.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/bootable/recovery
+git push --delete https://github.com/OMAP4-AOSP/android_bootable_recovery.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/build
+git push --delete https://github.com/OMAP4-AOSP/android_build.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/frameworks/av
+git push --delete https://github.com/OMAP4-AOSP/android_frameworks_av.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/frameworks/base
+git push --delete https://github.com/OMAP4-AOSP/android_frameworks_base.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/frameworks/native
+git push --delete https://github.com/OMAP4-AOSP/android_frameworks_native.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/hardware/ril
+git push --delete https://github.com/OMAP4-AOSP/android_hardware_ril.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/packages/apps/Camera2
+git push --delete https://github.com/OMAP4-AOSP/android_packages_apps_Camera2.git $1
+echo ""
+echo "----------------------------------------"
+cd $SAUCE/system/core
+git push --delete https://github.com/OMAP4-AOSP/android_system_core.git $1
+echo ""
+echo "----------------------------------------"
+echo "----------------------------------------"
+echo ""
+echo "Done!"
+cd $SAUCE
+echo "----------------------------------------"
+echo "----------------------------------------"
